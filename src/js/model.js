e2a37c2460f7c37db4c8ed70b6d4fd6240806226
sergeylitvinc4w <plzAJ08iP26EIl6ftMx3>
2020-11-19 00:45:33 +0200

    Change text in model and add styles.

--- src/js/model.js
+++ src/js/model.js
@@ -1,7 +1,10 @@
 import image from '../assets/image.png';
 
 export const model = [
-    {type: 'title', value: 'Constructor sites on Javascript!'},
+    {type: 'title', value: 'Constructor sites on pure Javascript!', options: {
+            tag: 'h1',
+            styles: `background: linear-gradient(to right, #ff0099, #493240); color: #ffffff; text-align: center; padding: 1.5rem;`
+        }},
     {type: 'text', value: 'here we go'},
     {
         type: 'columns', value: [

5b3a16b916a561204939dc97429cb6fc94eb6215
sergeylitvinc4w <plzAJ08iP26EIl6ftMx3>
2020-12-27 13:19:32 +0200

    Create App class and methods

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import { model } from './js/model';
-import { Site } from './classes/site';
+import {App} from "./classes/app";
 import './styles/main.css';
 
-const site = new Site('#site');
 
-site.render(model);
+new App(model).init();
+
 
 
 
